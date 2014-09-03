5b22a2e54562a9cd4347fc88aeb3cd000ca1cfbc
brian <bmbove@gmail.com>
2014-09-02 23:04:44 -0700

    working url title grab

--- irc.py
+++ irc.py
@@ -3,79 +3,43 @@ import time
 import re
 import traceback
 from threading import Thread
+from Queue import Queue
 from command import BasicCmd
-# from plugins import *
+import urllib2
+import HTMLParser
 
 
-class IRCBot(Thread):
+class IRCBot():
 
-    # Usage- requires server, port, a read queue and a write queue.
-    # Messages from the server will be added to the read queue, with
-    # each message ending in typical IRC \r\n
-    # Messages put into the write queue are things that need to be
-    # sent to the server. The handler function for the write queue
-    # accepts a set of commands, each in a list:
-    # ["join", channel_name]
-    # ["part", channel_name]
-    # ["quit"]
-    # ["topic", channel_name, new_topic]
-    # ["say", channel/nick, message]
-    # ["who", channel/nick]
-    # It will send arbitrary commands with:
-    # ["raw", command] (no \r\n is needed on the end of the command
-    # ["delay", time] sleeps for time seconds
-    # The init can also take a list of channels to autojoin once a
-    # connection to the server is established.
-    # Thread will attempt to auto-reconnect on ping timeout
-
-    def __init__(self, server, port, readq, writeq, *args, **kwargs):
-        # Decides whether to delay in connecting
+    def __init__(self, server, port, *args, **kwargs):
         self.reconnect = False
-        # socket for irc communication
         self.sock = False
-        # server address
         self.server = server
-        # server port
         self.port = port
-        # whether we're connected or not
+        #self.botqueue = botqueue
         self.connected = False
-        # read queue - messages from the server
-        # and stuff we may want to display
-        self.readq = readq
-        # write queue - we try to send everything
-        # in here to the server in one way or another
-        self.writeq = writeq
-        # whether we've joined the initial channels
         self.joined = False
-        # last time we responded to a ping from the server
-        self.last_ping = time.time()
-        # set nickname
-        self.bot_nick = "prickbot"
-        # build a list of channels to join initially
+        self.bot_nick = "prickbot_n"
         self.channels = []
+        self.send_q = Queue()
         if "channels" in kwargs:
             self.channels = kwargs['channels']
 
-        parser = IRCParse(readq, writeq, bot_nick=self.bot_nick)
-        parser.start()
-
-        super(IRCBot, self).__init__()
-
     def connect(self):
+
         if self.sock:
             self.sock.close()
-        s = socket.socket()
+
         if self.reconnect:
-            msg = "Reconnecting to %s in 30 seconds..." % self.server
-            self.readq.put(msg)
+            print("Reconnecting to %s in 30 seconds..." % self.server)
             self.joined = False
             self.last_ping = time.time()
             time.sleep(30)
         else:
-            msg = "Connecting to %s ..." % self.server
-            self.readq.put(msg)
-        connected = False
+            print("Connecting to %s ..." % self.server)
 
+        s = socket.socket()
+        connected = False
         while not connected:
             try:
                 s.connect((self.server, self.port))
@@ -85,8 +49,8 @@ class IRCBot(Thread):
                 print e
                 connected = False
 
-        user_str = "USER " + self.bot_nick + " blah.net * :" + self.bot_nick
-        nick_str = "NICK " + self.bot_nick
+        user_str = "USER %s blah.net * :%s" % (self.bot_nick, self.bot_nick)
+        nick_str = "NICK %s" % self.bot_nick
 
         s.send(user_str + "\r\n")
         s.send(nick_str + "\r\n")
@@ -95,225 +59,105 @@ class IRCBot(Thread):
         self.connected = connected
 
     def run(self):
-        self.exitapp = False
-        while not self.exitapp:
+        while True:
             if not self.connected:
                 self.connect()
 
+            if not self.send_q.empty():
+                self.sock.send(self.send_q.get().encode('utf-8'))
+                self.send_q.task_done()
+
             message = ""
-
-            if not self.writeq.empty():
-                self.writeq_handle(self.writeq.get())
-                self.writeq.task_done()
-
             # Grab 1 byte at a time until a full message is in the buffer
             while "\r\n" not in message:
                 # if there's nothing to receive, break outta here
                 try:
-                    new_msg = self.sock.recv(1)
-                    message = message + str(new_msg)
+                    message += str(self.sock.recv(1))
                 except:
                     break
+
+            if "/MOTD" in message and self.joined is False:
+                for channel in self.channels:
+                    self.send_q.put("JOIN %s\r\n" % channel)
+                self.joined = True
+
             # if we've got a message, handle it
             if message != "":
-                self.readq.put(message)
-                self.readq_handle(message)
-        exit(0)
+                parser = IRCParse(self.send_q, message, self.bot_nick)
+                parser.start()
 
-    def channel_join(self, msg):
-        channel = msg[1]
-        self.readq.put("Joining %s" % channel)
-        self.sock.send("JOIN %s\r\n" % channel)
-
-    def channel_leave(self, msg):
-        channel = msg[1]
-        if len(msg) > 2:
-            reason = msg[2]
-        else:
-            reason = 'and I\'m out!'
-        self.readq.put("Leaving %s" % channel)
-        command = "PART %s :\"%s\"\r\n" % (channel, reason)
-        self.readq.put(command)
-        self.sock.send(command)
-
-    def who_list(self, msg):
-        entity = msg[1]
-        self.readq.put("Getting WHO listing for %s" % entity)
-        self.sock.send("WHO %s\r\n" % entity)
-
-    def set_topic(self, msg):
-        channel = msg[1]
-        topic = msg[2]
-        self.readq.put("Setting new topic for %s: %s" % (channel, topic))
-        self.sock.send("TOPIC %s %s\r\n" % (channel, topic))
-
-    def say(self, msg):
-        entity = msg[1]
-        message = msg[2]
-        name = self.bot_nick
-        # print out what we're saying, useful for logging
-        leader = ":%s!~%s@%s PRIVMSG %s :%s" % (name,
-                                                name,
-                                                name,
-                                                entity,
-                                                message)
-        self.readq.put(leader)
-        self.sock.send("PRIVMSG %s :%s\r\n" % (entity, message))
-
-    def quit_irc(self, msg):
-        self.sock.send("QUIT :I'll be back" + "\r\n")
-        self.readq.put("exit")
-        self.exitapp = True
-
-    def raw_send(self, msg):
-        self.sock.send(msg[1] + "\r\n")
-
-    def delay(self, msg):
-        time.sleep(msg[1])
-
-    def writeq_handle(self, msg):
-        cmds = {'say': self.say,
-                'join': self.channel_join,
-                'part': self.channel_leave,
-                'topic': self.set_topic,
-                'who': self.who_list,
-                'quit': self.quit_irc,
-                'raw': self.raw_send,
-                'delay': self.delay,
-                }
-        try:
-            if msg[0] in cmds:
-                command = cmds[msg[0]]
-                command(msg)
-        except Exception, e:
-            print traceback.format_exc()
-            print e
-
-    def readq_handle(self, msg):
-
-        # Check for Ping- response with Pong and note the time
-        message_list = msg.split(" ")
-        if message_list[0] == "PING":
-            self.sock.send("PONG " + message_list[1] + "\r\n")
-            self.readq.put("PONG")
-            self.last_ping = time.time()
-
-        lp_time = time.time() - self.last_ping
-        # if we haven't been pinged in over 5 min, we probably
-        # lost connection. Set some stuff to try to reconnect
-        if lp_time > 330:
-            msg = "Lost Connection - Ping timeout: %s seconds" % str(lp_time)
-            self.readq.put(msg)
-            self.connected = False
-            self.reconnect = True
-
-        # If channels are passed in the init, join them now
-        if "/MOTD" in msg and self.joined is False:
-            for channel in self.channels:
-                self.writeq.put(['join', channel])
-                self.joined = True
 
 
 class IRCParse(Thread):
 
-    def __init__(self, readq, writeq, *args, **kwargs):
-        # read queue - messages from the server
-        # and stuff we may want to display
-        self.readq = readq
-        # write queue - we try to send everything
-        # in here to the server in one way or another
-        self.writeq = writeq
-        # latest message string from IRC server
-        self.msg = ''
-        # bot nickname
-        if 'bot_nick' in kwargs:
-            self.bot_nick = kwargs['bot_nick']
-        else:
-            self.bot_nick = 'prickbot'
-        # start up instance of basic commands
-        self.basic = BasicCmd()
+    def __init__(self, send_q, message, nick, *args, **kwargs):
+        self.send_q = send_q 
+        self.bot_nick = nick
+        self.message = message.strip()
         super(IRCParse, self).__init__()
 
     def run(self):
-        self.exitapp = False
-        while not self.exitapp:
-            try:
-                self.msg = ""
-                # check for new stuff to read/parse
-                if not self.readq.empty():
-                    self.msg = self.readq.get().strip()
-                    self.readq.task_done()
-                    print self.msg
-                    # exit the thread if told to do so
-                    if self.msg == "exit":
-                        self.exitapp = True
-                    # parse response, formulate witty reply
-                    reply = self.parse_msg()
-                    # if we were clever enough, write reply to server
-                    if reply:
-                        for cmd in reply:
-                            self.writeq.put(cmd)
-            except Exception, e:
-                print traceback.format_exc()
-                print e
-        exit(0)
-
-    def parse_msg(self):
-
-        # regex match various parts of message
-        re_str = (""
-                  "^:(?P<nick>(\S*))!~(?P<user>(\S*))@(?P<host>(\S*)) "
-                  "(?P<command>([A-Z+])*) (?P<entity>([&#-_A-z0-9]+)?) "
-                  ":?(?P<message>(.+)?$)"
-                  )
-        p = re.compile(re_str)
-        m = p.match(self.msg)
-
-        if m:
-            msg_array = m.groupdict()
-            self.msg_array = msg_array
-            if self.bot_nick in msg_array['nick']:
-                return False
-            elif msg_array['message'][0:1] == "!":
-                msg_split = msg_array['message'].split(' ')
-                command = msg_split[0][1:]
-                self.msg_array['param_str'] = ''
-                self.msg_array['param_l'] = []
-                for i in range(1, len(msg_split)):
-                    self.msg_array['param_str'] += msg_split[i] + " "
-                    self.msg_array['param_l'].append(msg_split[i])
-                return self.interpret_command(command)
-            elif self.url_matcher():
-                url = self.url_matcher()
-                title = self.basic.grab_title(url)
-                cmd = ['say', msg_array['entity'], title]
-                return [cmd]
-            else:
-                return False
-        else:
-            return False
-
-    def interpret_command(self, command):
         try:
-            cmd_list = {}
-            basic_list = self.basic.avail_cmds
-
-            for key, value in basic_list.iteritems():
-                cmd_list[key] = value[0]
-            if command in cmd_list:
-                response = self.basic.run(self.msg_array, command)
-                return response
+            print self.message
+            self.message_handle(self.message)
         except Exception, e:
             print traceback.format_exc()
             print e
-            return [['say', self.msg_array['entity'], e]]
+
+    def say(self, msg):
+        entity = msg[1]
+        message = msg[2]
+        m_message = []
+        while len(message) > 450:
+            m_message.append(message[0:450])
+            message = message[450:]
+        m_message.append(message)
+        for message in m_message:
+            leader = ":%s!~%s@%s PRIVMSG %s :%s" % (
+                self.bot_nick,
+                self.bot_nick,
+                self.bot_nick,
+                entity,
+                message
+            )
+            print(leader)
+            self.send_q.put("PRIVMSG %s :%s\r\n" % (entity, message))
+
+    def message_handle(self, message):
+        # Check for Ping- response with Pong and note the time
+        message_list = message.split(" ")
+        if message_list[0] == "PING":
+            self.send_q.put("PONG %s\r\n" % message_list[1])
+            return True 
+
+        re_str = (
+            ""
+            "^:(?P<nick>(\S*))!~(?P<user>(\S*))@(?P<host>(\S*)) "
+            "(?P<command>([A-Z+])*) (?P<entity>([&#-_A-z0-9]+)?) "
+            ":?(?P<message>(.+)?$)"
+        )
+
+        p = re.compile(re_str)
+        m = p.match(self.message)
+
+        if m:
+            self.message_dict = m.groupdict()  
+            if self.bot_nick in self.message_dict['nick']:
+                return False
+            elif self.url_matcher():
+                url = self.url_matcher()
+                title = self.grab_title(url)
+                self.say(['say', self.message_dict['entity'], title]) 
+                return True
+        else:
+            return False
 
     def url_matcher(self):
-        message = self.msg_array['message']
+        message = self.message_dict['message']
         re_str = (""
                   "(?:https?://|www\.)"
-                  "[\w\-\@;\/?:&=%\$_.+!*\x27(),~#]+"
-                  "[\w\-\@;\/?&=%\$_+!*\x27()~]"
+                  "[0-9\.\w\-\@;\/?:&=%\$_.+!*\x27(),~#]+"
+                  "[0-9\.\w\-\@;\/?&=%\$_+!*\x27()~]"
                   )
         p = re.compile(re_str)
         m = p.search(message)
@@ -321,3 +165,28 @@ class IRCParse(Thread):
             return m.group()
         else:
             return False
+
+    def grab_title(self, url): 
+        if url[0:7] != "http://" and url[0:8] != "https://":
+            url = "http://" + url
+        response = self.grab_page(url)
+        re_string = "<title>(.*?)<\/title>"
+        p = re.compile(re_string, re.DOTALL | re.M)
+        m = p.search(response)
+        h = HTMLParser.HTMLParser()
+        title = m.groups()[0].strip()
+        title = title.replace("\n", "")
+        title = title.replace("\r", "")
+        title = title.replace("\t", "    ")
+        title_s = "Title: %s" % h.unescape(title.decode('utf-8'))
+        return title_s
+
+    def grab_page(self, url):
+
+        ua = ("Mozilla/5.0 (X11; Linux x86_64; rv:31.0)"
+            " Gecko/20100101 Firefox/31.0")
+        headers = { 'User-Agent' : ua }
+        req = urllib2.Request(url, None, headers)
+        response = urllib2.urlopen(req)
+
+        return response.read()
--- prickbot.py
+++ prickbot.py
@@ -1,33 +1,9 @@
-from Queue import Queue
-from irc import IRCBot, IRCParse
+from irc import IRCBot
 
-writeq = Queue()
-readq = Queue()
 ttl = False
 
-
-def main():
-    global writeq, readq
-    channels = ["#pricktest"]
-    bot = IRCBot('irc.freenode.net',
-                 6667,
-                 readq,
-                 writeq,
-                 channels=channels)
-    bot.start()
-
-    parser = IRCParse(readq, writeq)
-    parser.start()
-
-    while not ttl:
-        pass
-    exit(0)
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        writeq.put(["quit"])
-        ttl = True
-        raise
-        exit(0)
+channels = ["#pricktest"]
+bot = IRCBot('irc.freenode.net',
+                6667,
+                channels=channels)
+bot.run()
